2b1013199b15f5f171c1db2e47242f07b84e6116
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-04-16 19:08:04 +0300

    configure table view

---
+++ Breweries list/Breweries list/BreweriesTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BreweriesTableViewCell.swift
+//  Breweries list
+//
+//  Created by Sergey Pohrebnuak on 16.04.2020.
+//  Copyright © 2020 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+class BreweriesTableViewCell: UITableViewCell {
+    
+    func setData() {
+        
+    }
+    
+}
--- Breweries list/Breweries list/ViewController.swift
+++ Breweries list/Breweries list/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet fileprivate weak var breweriesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //register table view cell
+        let nib = UINib.init(nibName: "BreweriesTableViewCell", bundle: nil)
+        breweriesTableView.register(nib, forCellReuseIdentifier: "BreweriesTableViewCell")
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BreweriesTableViewCell") as! BreweriesTableViewCell
+        cell.setData()
+        return cell
+    }
+    
+    
+}
