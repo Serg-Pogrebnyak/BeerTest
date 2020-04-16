7c1bcd998d7091e58715fc7c44f3cfa709e13ae2
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2020-04-16 18:52:36 +0300

    initial commit

---
+++ Breweries list/Breweries list/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Breweries list
+//
+//  Created by Sergey Pohrebnuak on 16.04.2020.
+//  Copyright © 2020 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
